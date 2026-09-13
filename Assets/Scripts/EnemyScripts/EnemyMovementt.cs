8924477ea3bc5a5f6ec4571b300692eb58f789df
Benja Wy <benjaminwysoki45@gmail.com>
2026-09-12 22:50:36 -0300

    enemy
    Cree el primer enemigo que siga al jugador. Todavía no funciona.

---
+++ Assets/Scripts/EnemyScripts/EnemyMovementt.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private int healt;
+    [SerializeField] private int speed;
+    [SerializeField] private Transform Player;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Player != null)
+        {
+            Debug.Log("holA");
+        Vector3 direction = (Player.transform.position-transform.position);
+        direction.Normalize();
+        transform.Translate(direction*speed*Time.deltaTime);
+        }
+        
+    }
+}
