efce2a5aac79dc25793d62d5e39b7bdfe980e2da
Providence-art <springtrapwins@gmail.com>
2026-09-15 15:44:30 -0300

    TextMesh pro añadido. Escena de inicio añadida junto con un botón

---
+++ Assets/Scripts/ItemScripts/ButtonScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonScript : MonoBehaviour
+{
+   public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+   
+     
+}
