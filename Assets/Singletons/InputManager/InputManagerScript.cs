55128a8c0e37fa08a396c177af6976d253453b6a
Providence-art <springtrapwins@gmail.com>
2026-09-16 15:53:23 -0300

    Última actualización del prototipo. Añadida escena final

--- Assets/Singletons/InputManager/InputManagerScript.cs
+++ Assets/Singletons/InputManager/InputManagerScript.cs
@@ -1,5 +1,6 @@
 using Microsoft.VisualBasic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 public class InputManagerScript : MonoBehaviour
 {
@@ -9,9 +10,11 @@ public class InputManagerScript : MonoBehaviour
     public InputActionReference crouchAction;
     public InputActionReference jumpAction;
 
+    public InputActionReference Reset;
+
     static private InputManagerScript instance;
     static public InputManagerScript Instance => instance;
-
+    private string currentSceneName;
         
     private void Awake()
     {
@@ -25,6 +28,21 @@ public class InputManagerScript : MonoBehaviour
             Destroy(gameObject);
         }
     }
+    private void OnEnable()
+    {
+        Reset.action.Enable();
 
-
+    }
+    private void OnDisable()
+    {
+        Reset.action.Disable();
+    }
+    private void Update()
+    {
+       bool resetting = Reset.action.IsPressed(); 
+        if (resetting == true){
+            currentSceneName = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentSceneName);
+        }
+    }
 }
